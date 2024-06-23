5df7bcaaafa3f844a4132e5a9b9d4dc9b0709a7f
ebutkev <eugeniabutkevich2022@gmail.com>
2024-06-23 20:06:47 +0200

    binary

--- challenge.js
+++ challenge.js
@@ -1,4 +1,5 @@
-const getMinMax = function (arr) {
+{
+  /*const getMinMax = function (arr) {
   const min = Math.min(...arr);
   const max = Math.max(...arr);
   return { min, max };
@@ -150,19 +151,263 @@ const people = [
 ];
 
 const jungPeople = people
-.filter(names => names.age < 25)
-.map(person => person.name)
+  .filter((names) => names.age < 25)
+  .map((person) => person.name);
 console.log(jungPeople);
 
+const numbersArray = [1, 3, 6, 7, 888, -34, -28, -4];
 
-const numbersArray = [1,3,6,7,888,-34,-28,-4];
+const sumOfPositive = numbersArray
+  .filter((posNum) => posNum > 0)
+  .reduce((acc, curr) => acc + curr, 0);
 
-const sumOfPositive = numbersArray.filter(posNum => posNum >0)
-.reduce((acc,curr)=>acc + curr, 0)
-
-
-console.log(sumOfPositive)
+console.log(sumOfPositive);
 
 const words = ["coder", "programmer", "developer"];
-const capitalizedWords = words. map(word=>word[0].toUpperCase() + word.slice(1))
+const capitalizedWords = words.map(
+  (word) => word[0].toUpperCase() + word.slice(1)
+);
 console.log(capitalizedWords);
+
+const fibonacci = (n) => {
+  if (n < 0) {
+    return [];
+  }
+
+  if (n === 1) {
+    return [0];
+  }
+
+  const fibsec = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fibsec.push(fibsec[i - 1] + fibsec[i - 2]);
+  }
+  return fibsec;
+};
+console.log(fibonacci(6));
+
+// binary search- serch the position of item within array
+
+const postion = (arr, target) => {
+  let first = 0;
+  let last = arr.length - 1;
+
+  while (first <= last) {
+    let mid = Math.floor((last + first) / 2);
+
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] < target) first = mid + 1;
+    else last = mid - 1;
+  }
+  return -1;
+};
+console.log(postion([1, 2, 3, 4, 5, 6, 87, 90], 90));
+
+//reverce string
+
+const reverceStr1 = (str) => {
+  if (str === "") return "";
+  else {
+
+    return reverceStr1(str.slice(1)) + str[0];
+  }
+};
+console.log(reverceStr1("hello"));
+
+//find in array
+ 
+const findInArr = (arr, target)=>{
+if (arr.lenfth < 1) return 
+for ( let i =0; i<= arr.length;
+  i++){
+    if (arr[i]===target) return i
+
+  }  return 0
+}
+console.log(findInArr([1,2,3,4,5,6,7],3));
+
+const findAgain =(arr, target)=>{
+let firstPos = 0;
+let lastPos = arr.length;
+
+
+while (firstPos <= lastPos){
+let mid = Math.floor((firstPos+lastPos)/2)
+if (arr[mid]===target) return mid;
+else if ( arr[mid]>target)  lastPos = mid -1;
+else firstPos = mid +1
+
+}
+return -1
+
+
+}
+console.log( findAgain([1,2,3,4,5,6,7,555],555));
+
+const arrayNum = [2,4,5,6,17,38,9,7,6,5,5]
+const newAr = new Set(arrayNum)
+const finalArr = Array.from(newAr)
+console.log(finalArr);
+
+
+const arr4 = finalArr.sort((a,b)=>b-a)
+console.log(arr4);
+const arr6 = [9,8,7,6,"(", "(", "()", "()", "[]", "]"]
+const arr5 = arr6.filter(x =>x !== "(" &&")")
+console.log(arr5);
+
+const finder = (arr,target)=>{
+if(arr.length ===0) return
+
+for (num of arr){
+  if (num === target) return  true
+}
+}
+console.log(finder([3,4,5,6,7,8,9], 8));
+
+const array9 = ["apple","potato", "lemon", "grass"];
+
+const findGrass = array9.includes("grass")
+console.log("hi",findGrass);
+
+const array9K = array9.keys()
+    for (let key of array9K){
+    console.log(key);
+  }
+
+const foodEntries = array9.entries()
+for (let entr of foodEntries ){
+  console.log(entr);
+}
+
+console.log( Array.isArray(array9)? "true":"false");*/
+}
+
+const ar = ["strowberry"];
+const array = ["apple", "banana", "potato", "kiwi"];
+
+array.forEach((item) => console.log(item));
+
+const res1 = array.map((item) => item.charAt(0).toUpperCase() + item.slice(1));
+console.log(res1);
+
+const res2 = array.filter((item) => item === "banana");
+console.log(res2);
+
+const res3 = array.concat(ar);
+console.log(res3);
+
+const res4 = array.find((item) => item === "kiwi");
+console.log(res4);
+
+const res5 = array.findIndex((item) => item === "kiwi");
+console.log(res5);
+
+console.log(array.indexOf("kiwi"));
+
+const arr6 = array.some((item) => item === "kiwi");
+console.log(arr6);
+const arr7 = array.every((item) => item === "dodg");
+console.log(arr7);
+
+console.log(array.includes("kiwi"));
+
+array.push("tomato");
+array.pop();
+array.shift(); //removes first el
+array.unshift("garlic");
+console.log(array);
+const stringifiedArr = array.toString();
+
+console.log(stringifiedArr);
+
+const joinedArr = array.join("+");
+console.log(joinedArr);
+
+const filledArr2 = array.fill("exit", 1, 3);
+
+console.log(filledArr2);
+
+console.log(array.copyWithin(2, 3));
+
+const slicedArr = array.slice(2);
+console.log(slicedArr);
+array.push("tomato", "garlic", "nuts");
+console.log(array);
+//const spliced = array.splice(2)
+//console.log(spliced);
+
+const sortedArray = array.sort((a, b) => a - b);
+console.log(sortedArray);
+
+const reversed = sortedArray.reverse();
+console.log("hello", reversed);
+
+const string = "hello, haha";
+const res6 = Array.from(string);
+console.log(res6);
+const arr8 = res6.join("").split(",");
+console.log(arr8);
+
+const arrayNum = [
+  { name: "John" },
+  {
+    name: "Sara",
+  },
+];
+const keyArr = arrayNum.keys()
+for (let key of keyArr){
+  console.log(key);
+}
+
+const entriesArr = arrayNum.entries()
+for (let entry of entriesArr){
+  console.log(entry);
+}
+const valuesErr = arrayNum.values()
+for (let value of valuesErr){
+  console.log(value);
+}
+
+const arrayLong  = ["ra", "ra", "sdsfs", "kooo"]
+arrRemovedDuplicates= new Set(arrayLong)
+console.log(arrRemovedDuplicates);
+
+//QUICK SORT
+
+const unsortedArr = [3,6,7,8,9,8,7,4,34,8,2,9,6,8,4,]
+
+const sortArray = (arr)=>{
+
+if (arr.length <1) return arr
+
+let pivot = arr[Math.floor(arr.length/2)]
+  let left = [];
+  let rigth = [];
+  let equal =[];
+
+  for (let item of arr){
+    if (item < pivot){
+      left.push(item)
+    }
+    else if (item > pivot){
+          rigth.push(item)
+    }
+  else equal.push(item)
+  }
+
+
+return sortArray(left).concat(equal,sortArray(rigth))
+}
+
+ console.log( sortArray(unsortedArr))
+
+ const sorted = unsortedArr.sort((a,b)=>a-b)
+ console.log(sorted);
+
+const arr12 = [1,2,3,4,5,6,7,8,9,8,6,5,4,3,2,7,4,5,5,6,6,6,6]
+const target =5
+const found = arr12.findIndex((item)=>item ===target);
+console.log(found);
+
+
